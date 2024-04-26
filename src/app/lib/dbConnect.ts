aaad040dbd925a78ac4b5b5e53a21243b7d62964
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-04-26 23:56:51 +0530

    db connection configured

---
+++ src/app/lib/dbConnect.ts
@@ -0,0 +1,26 @@
+import mongoose
+from "mongoose";
+
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+
+const connection: ConnectionObject = {};
+
+async function dbConnect (): Promise<void>{
+
+    if(connection.isConnected){
+        console.log("already connected to dataabse")
+        return;
+    }
+    try {
+     const db =     await mongoose.connect(process.env.MONGODB_URI || '',{})
+     console.log(db)
+    connection.isConnected =  db.connections[0].readyState
+    console.log("Db connected successfully")
+    } catch (error) {
+        console.log("database connection faiiled",error)
+        process.exit(1);
+    }
+}
---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const messageSchema = z.object({
+   content:z
+   .string()
+   
+   .min(10,"Message must be atleast 10 character long")
+   .max(300,"Message must be atmost 300 characters long")
+
+})
