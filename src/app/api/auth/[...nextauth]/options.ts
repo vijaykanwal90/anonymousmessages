2e7a3f9ab9498d5709671bd8c3c856cade711f02
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-04-28 00:59:18 +0530

    set up sign in route

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,51 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { dbConnect } from "@/app/lib/dbConnect";
+import UserModel from "@/app/models/User.model";
+
+
+
+export const authOptions: NextAuthOptions = {
+    providers:[
+        CredentialsProvider(
+            {
+                id:"credentials",
+                name:"Credentials",
+                credentials:{
+                    email:{ label:"Email", type:"text"},
+                    password:{label:"Passowrd" , types:"password"}
+                },
+                async authorize(credentials:any):Promise<any>{
+                    await dbConnect();
+                    try {
+                      const user =   await UserModel.findOne({
+                            $or:[
+                                {email:credentials.identifier},
+                                {username:credentials.identifier}
+                            ]
+                        })
+                        if(!user){
+                            throw new Error("no user found with this email")
+                        }
+                        if(!user.isVerified){
+throw new Error('please verify your email before login')
+                        }
+                    const isPasswordCorrect =     await bcrypt.compare(user.password , credentials.passowordf)
+                    if(isPasswordCorrect){return user;}
+                    else {
+                        throw new Error('password is incorrect')
+                    }
+                    } catch (err:any) {
+                        throw new Error(err)
+                    }
+                }
+
+            }
+        )
+
+    ],
+    pages:{
+        signIn:'sign-in'
+    }
+}
