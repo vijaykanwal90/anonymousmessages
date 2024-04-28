0f2df9cb23be281092a8dd9ae9b595dc69c420e4
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-04-28 14:21:01 +0530

    modifying session and token

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -7,36 +7,36 @@ import UserModel from "@/app/models/User.model";
 
 
 export const authOptions: NextAuthOptions = {
-    providers:[
+    providers: [
         CredentialsProvider(
             {
-                id:"credentials",
-                name:"Credentials",
-                credentials:{
-                    email:{ label:"Email", type:"text"},
-                    password:{label:"Passowrd" , types:"password"}
+                id: "credentials",
+                name: "Credentials",
+                credentials: {
+                    email: { label: "Email", type: "text" },
+                    password: { label: "Passowrd", types: "password" }
                 },
-                async authorize(credentials:any):Promise<any>{
+                async authorize(credentials: any): Promise<any> {
                     await dbConnect();
                     try {
-                      const user =   await UserModel.findOne({
-                            $or:[
-                                {email:credentials.identifier},
-                                {username:credentials.identifier}
+                        const user = await UserModel.findOne({
+                            $or: [
+                                { email: credentials.identifier },
+                                { username: credentials.identifier }
                             ]
                         })
-                        if(!user){
+                        if (!user) {
                             throw new Error("no user found with this email")
                         }
-                        if(!user.isVerified){
-throw new Error('please verify your email before login')
+                        if (!user.isVerified) {
+                            throw new Error('please verify your email before login')
                         }
-                    const isPasswordCorrect =     await bcrypt.compare(user.password , credentials.passowordf)
-                    if(isPasswordCorrect){return user;}
-                    else {
-                        throw new Error('password is incorrect')
-                    }
-                    } catch (err:any) {
+                        const isPasswordCorrect = await bcrypt.compare(user.password, credentials.passowordf)
+                        if (isPasswordCorrect) { return user; }
+                        else {
+                            throw new Error('password is incorrect')
+                        }
+                    } catch (err: any) {
                         throw new Error(err)
                     }
                 }
@@ -45,7 +45,30 @@ throw new Error('please verify your email before login')
         )
 
     ],
-    pages:{
-        signIn:'sign-in'
+    callbacks:{
+        async jwt({token , user }){
+            if(user){
+                token._id = user._id?.toString()
+                token.isVerified = user.isVerified
+                token.isAcceptingMessages = user.isAcceptingMessages
+                token.username = user.username
+            }
+
+return token 
+        },
+    
+async session({session, token}){
+    if(token){
+        session.user._id=token._id
     }
+return session 
+},
+},
+    pages: {
+        signIn: '/sign-in'
+    },
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET,
 }
---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,20 @@
+import 'next-auth' 
+
+
+declare module 'next-auth'{
+    interface User{
+        _id?:string;
+        isVerified?:boolean;
+        isAcceptingMessages?:boolean;
+        username?:string;
+    }
+    interface Session{
+        user:{
+            _id?:string;
+            isVerified?:boolean;
+            isAcceptingMessages?:boolean;
+            username?:string;
+    } & defaultSession['user']
+}
+    
+}
