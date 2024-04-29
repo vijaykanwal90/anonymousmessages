2ca645e979424f5f7fdcb435d67aea8ba024cae9
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-04-29 15:26:08 +0530

    username validation done

---
+++ src/app/api/verifyCode/route.ts
@@ -0,0 +1,56 @@
+import dbConnect from "@lib/dbConnect"
+import UserModel from  "@/model/User.model"
+
+export async function POST(request: Request){
+await  dbConnect()
+try {
+    
+
+    const {username , code} = await request.json()
+
+   const decodedUsername =  decodeURIComponent(username)
+   const user = await UserModel.findOne({username:decodedUsername})
+   if(!user){
+    return Response.json(
+        {
+            success:false,
+            messge:"user not found"
+         } , {status:500})
+        
+    
+   }
+   const isCodeValid = user.code === code
+   const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
+   if(isCodeValid && isCodeNotExpired){
+user.isVerified = true
+await user.save()
+return Response.json(
+    {
+        success:true,
+        messge:"account verified"
+     } , {status:200})
+   }
+   else if(!isCodeNotExpired){
+   return  Response.json(
+        {
+            success:false,
+            messge:"verification code expired , please sign again to get a new code"
+         } , {status:500})
+   }
+   else {
+   return  Response.json(
+        {
+            success:false,
+            messge:"incorrect verification code "
+         } , {status:400})
+   }
+} catch (error) {
+    console.error("error verifying user",error)
+    return Response.json(
+        {
+            success:false,
+            messge:"errro checking username"
+         } , {status:500})
+
+}
+}
