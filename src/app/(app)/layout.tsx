f6b2ccd992e6ca7507f9192d17247df71aa4365f
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-05-06 14:14:48 +0530

    error occuring

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+// import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 import AuthProvider from "@/context/AuthProvider";
