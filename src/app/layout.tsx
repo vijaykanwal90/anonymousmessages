ca3af19751a9ec08961ac93411af7fcd805e1d26
vijaykanwal90 <vijaykanwal90@gmail.com>
2024-05-06 23:34:02 +0530

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import AuthProvider from '../context/AuthProvider';
 import { Toaster } from '@/components/ui/toaster';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" >
       <AuthProvider>
+        <Navbar/>
         <body className={inter.className}>
           {children}
           <Toaster />
